8cc153f3d4a28f11e21b00bcdee89dbd43bd5b21
Sam Clements <sam@borntyping.co.uk>
2012-08-07 19:02:25 +0100

    Corrected formatting in setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ from setuptools import setup, find_packages
 from diceroll import __version__
 
 setup(
-    name             = 'diceroll',
-    version          = __version__,
+	name             = 'diceroll',
+	version          = __version__,
 
-    author           = 'Sam Clements',
-    author_email     = 'sam@borntyping.co.uk',
-    url              = 'https://github.com/borntyping/diceroll',
-    
-    description      = 'A command line dice roller',
-    long_description = open('README.rst').read(),
-    
-    packages         = find_packages(),
-    install_requires = ['pyparsing>=1.5.6'],
-    
-    entry_points = {'console_scripts': [
+	author           = 'Sam Clements',
+	author_email     = 'sam@borntyping.co.uk',
+	url              = 'https://github.com/borntyping/diceroll',
+	
+	description      = 'A command line dice roller',
+	long_description = open('README.rst').read(),
+	
+	packages         = find_packages(),
+	install_requires = ['pyparsing>=1.5.6'],
+	
+	entry_points = {'console_scripts': [
 		'roll  = diceroll:command',
 	]},
 )
