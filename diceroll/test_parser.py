661bd3ada08ed50792420800a80bc799a40b8c2c
Sam Clements <sam@borntyping.co.uk>
2012-11-25 11:56:00 +0000

    Use Argumented for tests

--- diceroll/test_parser.py
+++ diceroll/test_parser.py
@@ -5,6 +5,9 @@ import unittest
 from diceroll import roll, ParseException
 from diceroll.components import RolledDice
 
+from argumented import unpack_arguments, argument, argument_list, argument_tuples
+from itertools import product
+
 class TestNumber (unittest.TestCase):
 	def test_parse (self):
 		self.assertEquals(roll("1"), 1)
@@ -33,30 +36,39 @@ class TestDice (unittest.TestCase):
 	
 	def test_missing_s (self):
 		self.assertRaises(ParseException, roll, "1d")
-	
+
+subatoms = ('6d6', '(6d6)')
+atoms = lambda *ops: ((x, {}) for x in product(ops, subatoms))
+
+@unpack_arguments
 class TestUnaryOperators (unittest.TestCase):
-	def setUp (self):
-		self.subatoms = ("6d6", "(6d6)")
+	@argument_list(*subatoms)
+	def test_subatoms (self, subatom):
+		self.assertIsInstance(roll(subatom), RolledDice)
 	
-	def test_subatoms (self):
-		for subatom in self.subatoms:
-			self.assertIsInstance(roll(subatom), RolledDice)
+	@argument_tuples(*atoms("x", "explode"))
+	def test_explode (self, op, subatom):
+		self.assertIsInstance(roll(subatom + op), RolledDice)
 	
-	def test_explode (self):
-		for subatom in self.subatoms:
-			for op in ("x", "explode"):
-				self.assertIsInstance(roll(subatom + op), RolledDice)
+	@argument_tuples(*atoms("s", "sort"))
+	def test_sort (self, op, subatom):
+		result = roll(subatom + op)
+		self.assertEquals(result, sorted(result))
 	
-	def test_sort (self):
-		for subatom in self.subatoms:
-			for op in ("s", "sort"):
-				result = roll(subatom + op)
-				self.assertEquals(result, sorted(result))
-			
-	def test_total (self):
-		for subatom in self.subatoms:
-			for op in ("t", "total"):
-				self.assertIsInstance(roll(subatom + op), int)
+	@argument_tuples(*atoms("t", "total"))
+	def test_total (self, op, subatom):
+		self.assertIsInstance(roll(subatom + op), int)
+	
+@unpack_arguments
+class TestBinaryOperators (unittest.TestCase):
+	
+	@argument_list('v', 'drop')
+	def test_drop (self, op):
+		self.assertEquals(len(roll("2d1 "+op+" 1")), 1)
+				
+	@argument_list('^', 'keep')
+	def test_keep (self, op):
+		self.assertEquals(len(roll("6d1 "+op+" 2")), 2)
 
 if __name__ == '__main__':
     unittest.main()
